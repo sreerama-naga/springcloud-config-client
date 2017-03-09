a65856dd9e04fe97c1353be67526e4a4e4e63948
sreerama-naga <sreerama.naga@gmail.com>
2017-03-09 14:16:16 +0530

    new changes

--- src/main/java/com/example/web/RateController.java
+++ src/main/java/com/example/web/RateController.java
@@ -2,11 +2,11 @@ package com.example.web;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RefreshScope
 public class RateController {
 	
